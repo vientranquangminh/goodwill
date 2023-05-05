e33987f1033a1e07fa7a8f414e6e69d16f04bc7f
vientranquangminh <vienquangminh14@gmail.com>
2023-05-05 18:25:24 +0700

    fix(build): fix web build

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,9 +1,7 @@
 import 'dart:ui';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:goodwill/firebase_options.dart';
-import 'package:goodwill/source/injection/injector.dart';
 
 class AppConfiguration {
   AppConfiguration._();
@@ -15,8 +13,6 @@ class AppConfiguration {
   static RegExp usernamePattern = RegExp(r'^[a-zA-Z\d_\-=@.]+$');
 
   static Future<void> ensureAppConfiguration() async {
-    await ScreenUtil.ensureScreenSize();
-    // await initDependenciesInjection();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
